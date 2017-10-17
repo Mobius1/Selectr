3fc3203f98527d2bef7189a81ef9206a4a79238a
Adrian <adrian.testaavila@gmail.com>
2017-10-17 00:27:43 -0400

    reorganize and expand tests add utility methods for creating example data (options) and Selectr instances

--- tests/index.js
+++ tests/index.js
@@ -1,37 +1,227 @@
+/**
+ * Selectr test suite.
+ *
+ * Each module() should be wrapped in an immediately-invoked function expression,
+ * in order to provide an isolated scope for the module.
+ */
 (function() {
-	'use strict';
-	var s = document.createElement('select');
+    'use strict';
+
+    /**
+     * Returns a new generic dataset for tests.
+     * @return {Array} A list of {value, text} objects
+     */
+    function exampleData() {
+        return [
+            { value: 'value-0', text: 'zero' },
+            { value: 'value-1', text: 'one' },
+            { value: 'value-2', text: 'two' },
+            { value: 'value-3', text: 'three' },
+            { value: 'value-4', text: 'four' },
+            { value: 'value-5', text: 'five' },
+            { value: 'value-6', text: 'six' },
+            { value: 'value-7', text: 'seven' },
+            { value: 'value-8', text: 'eight' },
+            { value: 'value-9', text: 'nine' }
+        ];
+    }
+
+    /**
+     * Creates a new Selectr instance for tests.
+     * Adds a __done__() method for cleanup when test is complete.
+     * @param {Object} config Constructor options; uses defaultData() if options.data is empty
+     * @param {HTMLSelectElement} el Base <select> element; creates a new one if empty
+     * @return {Selectr} The new Selectr object
+     */
+    function newSelectr( config, el ) {
+        el = el || document.createElement( "select" );
+        if ( !(el.nodeName && el.nodeName === "SELECT") ) {
+            throw "'el' must be an HTMLSelectElement";
+        }
+        document.body.appendChild( el );
+
+        config = config || {};
+        config.data = config.data || exampleData();
+
+        var selectr = new Selectr( el, config );
+        selectr.__done__ = function () {
+            document.body.removeChild( selectr.container );
+        };
+        return selectr;
+    }
+
+    /**
+     * Basic tests.
+     */
+    (function () {
+        QUnit.module('General');
+
+        QUnit.test( "instantiation", function( assert ) {
+            var s = document.createElement( "select" );
+            document.body.appendChild( s );
+            var selectr = new Selectr( s, { data: exampleData() } );
+
+            assert.equal( typeof selectr, "object", "can create new instance" );
+            assert.equal( selectr.el, s, "instance has reference to <select> element" );
+        });
+
+        QUnit.test( "api methods", function( assert ) {
+            var selectr = newSelectr();
+            [
+                "add",
+                "clear",
+                "close",
+                "disable",
+                "enable",
+                "getValue",
+                "open",
+                "remove",
+                "removeAll",
+                "reset",
+                "search",
+                "serialize",
+                "setValue",
+                "toggle"
+            ].forEach( function (method) {
+                assert.equal( typeof selectr[method], "function", method + "() method exists" );
+            });
+
+            selectr.__done__();
+        });
+    })();
+
+    /**
+     * Tests for API methods.
+     */
+    (function () {
+        QUnit.module( "API Methods" );
+
+        QUnit.test( "setValue(), getValue()", function ( assert ) {
+            var oneSelectr = newSelectr();
+
+            assert.equal(
+                oneSelectr.getValue(),
+                "value-0",
+                "select-one: first option is selected by default"
+            );
+
+            oneSelectr.setValue( "value-2" );
+            assert.equal(
+                oneSelectr.getValue(),
+                "value-2",
+                "select-one: sets and gets a single value"
+            );
+
+            oneSelectr.__done__();
 
 
-	document.body.appendChild(s);
+            var multiSelectr = newSelectr( { multiple: true } );
 
-	var selector = new Selectr(s, {
-		data: [
-			{ value: 'value-1', text: 'Value 1' },
-			{ value: 'value-2', text: 'Value 2', selected: true },
-			{ value: 'value-3', text: 'Value 3' },
-			{ value: 'value-4', text: 'Value 4' },
-			{ value: 'value-5', text: 'Value 5', selected: true },
-			{ value: 'value-6', text: 'Value 6' },
-			{ value: 'value-7', text: 'Value 7' },
-			{ value: 'value-8', text: 'Value 8' },
-			{ value: 'value-9', text: 'Value 9' },
-			{ value: 'value-10', text: 'Value 10' }
-		],
-		multiple: true
-	});
+            assert.deepEqual(
+                multiSelectr.getValue(),
+                [],
+                "select-multi: no option is selected by default"
+            );
 
-	QUnit.module('General');
-	QUnit.test( "init", function( assert ) {
-		assert.ok( Object.prototype.toString.call(selector) === '[object Object]', "Passed!" );
-	});
-	QUnit.test( "selected", function( assert ) {
-		assert.ok( selector.options[1].selected === true && selector.options[4].selected === true, "Passed!" );
-	});
-	QUnit.test( "not-selected", function( assert ) {
-		assert.ok( selector.options[0].selected === false && selector.options[2].selected === false, "Passed!" );
-	});
-	QUnit.test( "select-one", function( assert ) {
-		assert.ok( selector.originalType === "select-one", "Passed!" );
-	});
+            multiSelectr.setValue( "value-1" );
+            assert.deepEqual(
+                multiSelectr.getValue(),
+                ["value-1"],
+                "select-multi: sets and gets a single value"
+            );
+            assert.deepEqual(
+                multiSelectr.getValue( true ),
+                { values: [{ text: "one", value: "value-1" }] },
+                "gets selected values in object format"
+            );
+            assert.deepEqual(
+                JSON.parse( multiSelectr.getValue( true, true ) ),
+                { values: [{ text: "one", value: "value-1" }] },
+                "gets selected values as json-encoded string"
+            );
+
+            multiSelectr.setValue( "value-1" );
+            assert.deepEqual(
+                multiSelectr.getValue(),
+                [],
+                "select-multi: deselects selected values"
+            );
+
+            multiSelectr.setValue( ["value-2", "value-5"] );
+            assert.deepEqual(
+                multiSelectr.getValue(),
+                ["value-2", "value-5"],
+                "select-multi: sets and gets multiple values"
+            );
+
+            multiSelectr.__done__();
+        });
+
+        // @todo tests for other public API methods:
+        // search
+        // add
+        // remove
+        // removeAll
+        // serialize
+        // open
+        // close
+        // toggle
+        // clear
+        // reset
+        // disable
+        // enable
+    })();
+
+    /**
+     * Tests for constructor options.
+     */
+    (function () {
+        QUnit.module( "Constructor Options" );
+
+        QUnit.test( "data", function ( assert ) {
+            var data = exampleData();
+            data[5].selected = true;
+            var selectr = newSelectr( { data: data } );
+
+            data.forEach( function ( option, index ) {
+                assert.equal(
+                    selectr.options[index].value,
+                    option.value,
+                    "option with value '" + option.value + "' exists"
+                );
+                assert.equal(
+                    selectr.options[index].textContent,
+                    option.text,
+                    "option with value '" + option.value + "' has expected label '" + option.text + "'"
+                );
+            });
+            assert.equal(
+                selectr.getValue(),
+                "value-5",
+                "option passed with 'selected' property is selected by default"
+            );
+
+            selectr.__done__();
+        });
+
+        // @todo tests for other constructor options/settings:
+        // defaultSelected
+        // multiple
+        // searchable
+        // clearable
+        // allowDeselect
+        // width
+        // placeholder
+        // maxSelections
+        // taggable
+        // tagSeperators
+        // tagPlaceholder
+        // renderOption
+        // renderSelection
+        // pagination
+        // nativeDropdown
+        // closeOnScroll
+        // sortSelected
+        // customClass
+    })();
 })();
