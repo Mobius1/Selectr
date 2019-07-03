02c0ba086251b4183693bd1e43f58264e3912c12
夏培旭 <18530815565@163.com>
2019-07-03 16:49:49 +0800

    add TypeScript type definition

--- index.d.ts
+++ index.d.ts
@@ -18,8 +18,8 @@ export interface IOptions {
 	tagSeperators?: string[];
 	tagPlaceholder?: string;
 	data?: IData[];
-	renderOption?: () => string;
-	renderSelection?: () => string;
+	renderOption?(): string;
+	renderSelection?(): string;
 	pagination?: number;
 	nativeDropdown?: boolean;
 	closeOnScroll?: boolean;
@@ -36,33 +36,33 @@ export interface IOptions {
 
 export default class Selectr {
 	constructor(select: HTMLSelectElement, options: IOptions);
-	setValue: (value: string | string[]) => void;
-	getValue: (toObject?: boolean, toJson?: boolean) => string | { text: string, value: string };
-	search: (query: string, anchor?: boolean) => IData[];
-	add: (data: IData | IData[]) => void;
-	remove: (data: number | string | number[] | string[]) => void;
-	removeAll: () => void;
-	serialize: (toJson?: boolean) => string | IData[];
-	open: () => void;
-	close: () => void;
-	toggle: () => void;
-	clear: () => void;
-	reset: () => void;
-	disable: () => void;
-	enable: () => void;
-	on(event: 'selectr.init', fun: () => void);
-	on(event: 'selectr.select', fun: (option: HTMLOptionElement) => void);
-	on(event: 'selectr.deselect', fun: (option: HTMLOptionElement) => void);
-	on(event: 'selectr.change', fun: (option: HTMLOptionElement) => void);
-	on(event: 'selectr.open', fun: () => void);
-	on(event: 'selectr.close', fun: () => void);
-	on(event: 'selectr.clear', fun: () => void);
-	on(event: 'selectr.reset', fun: () => void);
-	on(event: 'selectr.paginate', fun: (data: {
+	public setValue(value: string | string[]): void;
+	public getValue(toObject?: boolean, toJson?: boolean): string | { text: string, value: string };
+	public search(query: string, anchor?: boolean): IData[];
+	public add(data: IData | IData[]): void;
+	public remove(data: number | string | number[] | string[]): void;
+	public removeAll(): void;
+	public serialize(toJson?: boolean): string | IData[];
+	public open(): void;
+	public close(): void;
+	public toggle(): void;
+	public clear(): void;
+	public reset(): void;
+	public disable(): void;
+	public enable(): void;
+	public on(event: 'selectr.init', fun: () => void): void;
+	public on(event: 'selectr.select', fun: (option: HTMLOptionElement) => void): void;
+	public on(event: 'selectr.deselect', fun: (option: HTMLOptionElement) => void): void;
+	public on(event: 'selectr.change', fun: (option: HTMLOptionElement) => void): void;
+	public on(event: 'selectr.open', fun: () => void): void;
+	public on(event: 'selectr.close', fun: () => void): void;
+	public on(event: 'selectr.clear', fun: () => void): void;
+	public on(event: 'selectr.reset', fun: () => void): void;
+	public on(event: 'selectr.paginate', fun: (data: {
 		items: number;
 		total: number;
 		page: number;
 		pages: number;
-	}) => void);
+	}) => void): void;
 }
 
